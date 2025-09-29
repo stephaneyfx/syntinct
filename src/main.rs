9c1532d8f064a0cdb2b0a8277fe93450016639da
Stephane Raux <stephaneyfx@gmail.com>
2025-09-29 11:41:04 -0500

    Update CLI to account for new and old themes

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,55 @@
+use clap::{builder::styling::AnsiColor, Args, Parser, Subcommand, ValueEnum};
+
+fn terminal_styles() -> clap::builder::Styles {
+    clap::builder::Styles::styled()
+        .header(AnsiColor::Green.on_default().bold())
+        .usage(AnsiColor::Green.on_default().bold())
+        .literal(AnsiColor::Blue.on_default().bold())
+        .placeholder(AnsiColor::Cyan.on_default().bold())
+}
+
+#[derive(Debug, Parser)]
+#[clap(author, styles = terminal_styles(), version)]
+struct AppArgs {
+    #[clap(subcommand)]
+    cmd: AppCommand,
+}
+
+#[derive(Debug, Subcommand)]
+enum AppCommand {
+    Generate(GenerateCommand),
+}
+
+#[derive(Args, Debug)]
+struct GenerateCommand {
+    theme: ThemeName,
+}
+
+#[derive(Clone, Debug, ValueEnum)]
+enum ThemeName {
+    Syntark,
+    Thematic,
+}
+
 fn main() {
-    let neovim_theme = syntinct::NeovimTheme::new(
-        "thematic",
-        &syntinct::ThematicTheme::dark(),
-        &syntinct::ThematicTheme::light(),
-    );
+    let args = AppArgs::parse();
+    match args.cmd {
+        AppCommand::Generate(cmd) => generate_theme(cmd),
+    }
+}
+
+fn generate_theme(cmd: GenerateCommand) {
+    let neovim_theme = match cmd.theme {
+        ThemeName::Syntark => syntinct::NeovimTheme::new(
+            "syntark",
+            &syntinct::SyntarkTheme::default(),
+            &syntinct::SyntarkTheme::default(),
+        ),
+        ThemeName::Thematic => syntinct::NeovimTheme::new(
+            "thematic",
+            &syntinct::ThematicTheme::dark(),
+            &syntinct::ThematicTheme::light(),
+        ),
+    };
     println!("{}", neovim_theme.to_lua_module());
 }
